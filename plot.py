84f4ee7771f13a639889c0e1993e26ec0bd6c5b7
Tudor <tudorf@uw.edu>
2020-07-02 17:52:41 -0700

    fixed both cars

--- plot.py
+++ plot.py
@@ -46,44 +46,57 @@ for topic, msg, t in bag.read_messages(topics=topics):
     elif topic == '/car24/camera/color/image_throttled':
         count += 1
         if count == 1: continue
-        pt24_T_w = transformerROS.fromTranslationRotation((ps_24[-1].x, ps_24[-1].y, ps_24[-1].z), (q_24.x, q_24.y, q_24.z, q_24.w))
+        q_temp = (q_24.x, q_24.y, q_24.z, q_24.w)
+        pt24_T_w = transformerROS.fromTranslationRotation((ps_24[-1].x, ps_24[-1].y, ps_24[-1].z), (-q_24.z, q_24.x, -q_24.y, -q_24.w))
+        fix24 = np.matrix([
+            [ 0, 0, 1, 0],
+            [ 0,-1, 0, 0],
+            [-1, 0, 0, 0],
+            [ 0, 0, 0, 1]
+        ])
+        pt24_T_w = np.matmul(pt24_T_w, fix24)
+
         x24_T_pt24 = translate_transform([0.5,0,0])
         x24_T_w = np.matmul(pt24_T_w, x24_T_pt24)
         plt.plot([xs_24[-1], x24_T_w[0,3]], [ys_24[-1], x24_T_w[1,3]], '.-g')
 
-        z24_T_pt24 = translate_transform([0,0,0.5])
-        z24_T_w = np.matmul(pt24_T_w, z24_T_pt24)
-        plt.plot([xs_24[-1], z24_T_w[0,3]], [ys_24[-1], z24_T_w[1,3]], '.-b')
-
         y24_T_pt24 = translate_transform([0,0.5,0])
         y24_T_w = np.matmul(pt24_T_w, y24_T_pt24)
-        print('24', y24_T_w[1,3], ys_24[-1])
+        plt.plot([xs_24[-1], y24_T_w[0,3]], [ys_24[-1], y24_T_w[1,3]], '.-m')
+        print('24y', y24_T_w[2,3], ps_24[-1].z)
+
+        z24_T_pt24 = translate_transform([0,0,0.5])
+        z24_T_w = np.matmul(pt24_T_w, z24_T_pt24)
+        plt.plot([xs_24[-1], z24_T_w[0,3]], [ys_24[-1], z24_T_w[1,3]], '.-c')
+        print('24z', z24_T_w[2,3], ps_24[-1].z)
 
         pt26_T_w = transformerROS.fromTranslationRotation((-ps_26[-1].x, ps_26[-1].y, ps_26[-1].z), (-q_26.x, q_26.y, -q_26.z, q_26.w))
-        fix = np.matrix([
+        fix26 = np.matrix([
             [-1, 0, 0, 0],
-            [ 0,-1, 0, 0],
-            [ 0, 0, 1, 0],
+            [ 0, 0,-1, 0],
+            [ 0, 1, 0, 0],
             [ 0, 0, 0, 1]
         ])
-        pt26_T_w = np.matmul(pt26_T_w, fix)
+        pt26_T_w = np.matmul(pt26_T_w, fix26)
         
 
         x26_T_pt26 = translate_transform([0.5,0,0])
         x26_T_w = np.matmul(pt26_T_w, x26_T_pt26)
         plt.plot([xs_26[-1], x26_T_w[0,3]], [ys_26[-1], x26_T_w[1,3]], '.-g')
 
-        z26_T_pt26 = translate_transform([0,0,0.5])
-        z26_T_w = np.matmul(pt26_T_w, z26_T_pt26)
-        plt.plot([xs_26[-1], z26_T_w[0,3]], [ys_26[-1], z26_T_w[1,3]], '.-b')
-
         y26_T_pt26 = translate_transform([0,0.5,0])
         y26_T_w = np.matmul(pt26_T_w, y26_T_pt26)
-        print('26', y26_T_w[1,3], ys_26[-1])
+        plt.plot([xs_26[-1], y26_T_w[0,3]], [ys_26[-1], y26_T_w[1,3]], '.-m')
+
+        z26_T_pt26 = translate_transform([0,0,0.5])
+        z26_T_w = np.matmul(pt26_T_w, z26_T_pt26)
+        plt.plot([xs_26[-1], z26_T_w[0,3]], [ys_26[-1], z26_T_w[1,3]], '.-c')
+        print('26', z26_T_w[2,3], ps_26[-1].z)
+
 
         print(count)
-        plt.plot(xs_24, ys_24, '.-r')
-        plt.plot(xs_26, ys_26, '.-b')
+        # plt.plot(xs_24, ys_24, '.-r')
+        # plt.plot(xs_26, ys_26, '.-b')
         plt.xlim([-3,3])
         plt.ylim([-3,3])
         plt.savefig('position' + str(count) + '.png')
