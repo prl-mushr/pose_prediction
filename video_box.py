d2010dd00aa90ccf83e858421fa74dcf56980cdd
Tudor <tudorf@uw.edu>
2020-07-31 11:18:29 -0700

    video for new data

--- video_box.py
+++ video_box.py
@@ -1,23 +1,16 @@
 import rosbag
 import numpy as np
-
 import tf
-import geometry_msgs.msg
-import sensor_msgs.msg
-import std_msgs.msg
-
 import rospy
-
+import cv2
+from cv_bridge import CvBridge
 from image_geometry import PinholeCameraModel
 
-from cv_bridge import CvBridge
-import cv2
-
-import matplotlib.pyplot as plt
 
 transformerROS = tf.TransformerROS()
 bridge = CvBridge()
 camModel = PinholeCameraModel()
+vid_out = cv2.VideoWriter('test' + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 9, (640,480))
 
 def translate_transform(p):
     t = np.eye(4)
@@ -44,109 +37,102 @@ def inverse_transform(t):
     t_inv[2,3] = p_inv[2]
     return t_inv
 
-def fixRotation(pt24_T_w):
-    fix = np.matrix([
-            [ 1, 0, 0, 0],
-            [ 0, 0, 1, 0],
-            [ 0,-1, 0, 0],
-            [ 0, 0, 0, 1]
-    ])
-    return np.matmul(pt24_T_w, fix)
-
 trackedPt_T_baselink = translate_transform([-0.058325, 0, 0.08125])
-baselink_T_trackedPt = inverse_transform(trackedPt_T_baselink)
-colorCam_T_baselink = translate_transform([0.012, 0.033, 0.068])
+colorCam_T_baselink = translate_transform([0.02, 0.033, 0.068])
 center_T_baselink = translate_transform([-0.015, 0.0, 0.003])
+baselink_T_trackedPt = inverse_transform(trackedPt_T_baselink)
 
-bag = rosbag.Bag('/home/tudorf/mushr/catkin_ws/src/learning-image-geometry/align.bag')
-topics = ['/car24/PoseStamped','/car26/PoseStamped','/car26/camera/color/camera_info','/car26/camera/color/image_throttled']
+bag = rosbag.Bag('/home/tudorf/mushr/catkin_ws/src/learning-image-geometry/car37_trim.bag')
+topics = ['/vrpn_client_node/car37/pose','/vrpn_client_node/car38/pose','/car37/camera/color/camera_info','/car37/camera/color/image_throttled', '/vrpn_client_node/car35/pose']
 
-ps_24 = []
-ps_26 = []
-camInfo_26 = []
-cam_26 = []
+ps_38 = []
+ps_37 = []
+camInfo = None
+cam_37 = []
 
 for topic, msg, t in bag.read_messages(topics=topics):
-    if topic == '/car24/PoseStamped':
-        ps_24.append((t, msg.pose))
-    elif topic == '/car26/PoseStamped':
-        ps_26.append((t, msg.pose))
-    elif topic == '/car26/camera/color/camera_info':
-        camInfo_26.append((t, msg))
-    elif topic == '/car26/camera/color/image_throttled':
-        cam_26.append((t, msg))
+    if topic == '/vrpn_client_node/car37/pose':
+        ps_37.append((t, msg.pose))
+    elif topic == '/vrpn_client_node/car38/pose':
+        ps_38.append((t, msg.pose))
+    elif topic == '/car37/camera/color/camera_info':
+        camInfo = msg
+    elif topic == '/car37/camera/color/image_throttled':
+        cam_37.append((t, msg))
 
-idx24 = 0
-idx26 = 0
-idxInfo = 0
-images = []
-img_size = (0, 0)
-delay = 0.1
-for idxImg in range(len(cam_26)):
-    if idxImg == 60: break
-    targetT = cam_26[idxImg][0]
-    while idx26< len(ps_26)-1   and ps_26[idx26][0]     < targetT + rospy.Duration.from_sec(delay): idx26 += 1
-    while idx24< len(ps_24)-1   and ps_24[idx24][0]     < targetT + rospy.Duration.from_sec(delay): idx24 += 1
-    while idxImg< len(cam_26)-1 and cam_26[idxImg][0]   < targetT:                                  idxImg += 1
+idx38 = 0
+idx37 = 0
 
-    # extract mocap pose data, cameraInfo, and images
-    ps24 = ps_24[idx24][1]
-    ps26 = ps_26[idx26][1]
-    camInfo26 = camInfo_26[idxInfo][1]
-    img26 = cam_26[idxImg][1]
+print('mocap37', len(ps_37))
+print('mocap38', len(ps_38))
+print('image37', len(cam_37))
+
+for idxImg in range(len(cam_37)):
+    targetT = cam_37[idxImg][0]
+    while idx37< len(ps_37)-1   and ps_37[idx37][0]   < targetT: idx37 += 1
+    while idx38< len(ps_38)-1   and ps_38[idx38][0]   < targetT: idx38 += 1
+
+    # pick closest mocap data, not next
+    if idx37 > 0 and targetT - ps_37[idx37-1][0] < ps_37[idx37][0] - targetT:
+        idx37 -= 1
+    if idx38 > 0 and targetT - ps_38[idx38-1][0] < ps_38[idx38][0] - targetT:
+        idx38 -= 1
     
     # unwrap mocap pose into position, orientation
-    pos24 = ps24.position
-    ori24 = ps24.orientation
-    pos26 = ps26.position
-    ori26 = ps26.orientation
+    pos37 = ps_37[idx37][1].position
+    ori37 = ps_37[idx37][1].orientation
+    pos38 = ps_38[idx38][1].position
+    ori38 = ps_38[idx38][1].orientation
 
     # create Transforms for TrackedPts w.r.t. World
-    pt24_T_w = transformerROS.fromTranslationRotation((pos24.x, pos24.y, pos24.z), (ori24.x, ori24.y, ori24.z, ori24.w))
-    pt24_T_w = fixRotation(pt24_T_w)
-    pt26_T_w = transformerROS.fromTranslationRotation((pos26.x, pos26.y, pos26.z), (ori26.x, ori26.y, ori26.z, ori26.w))
-    pt26_T_w = fixRotation(pt26_T_w)
+    pt37_T_w = transformerROS.fromTranslationRotation((pos37.x, pos37.y, pos37.z), (ori37.x, ori37.y, ori37.z, ori37.w))
+    pt38_T_w = transformerROS.fromTranslationRotation((pos38.x, pos38.y, pos38.z), (ori38.x, ori38.y, ori38.z, ori38.w))
 
     # create Transforms for points of interest w.r.t. World
-    base24_T_w = np.matmul(pt24_T_w, baselink_T_trackedPt)
-    center24_T_w = np.matmul(base24_T_w, center_T_baselink)
-    base26_T_w = np.matmul(pt26_T_w, baselink_T_trackedPt)
-    cam26_T_w = np.matmul(base26_T_w, colorCam_T_baselink)
+    base37_T_w = np.matmul(pt37_T_w, baselink_T_trackedPt)
+    cam37_T_w = np.matmul(base37_T_w, colorCam_T_baselink)
+    base38_T_w = np.matmul(pt38_T_w, baselink_T_trackedPt)
+    center38_T_w = np.matmul(base38_T_w, center_T_baselink)
 
     # create Transforms for points of interest w.r.t. Camera
-    w_T_cam26 = inverse_transform(cam26_T_w)
-    center24_T_cam26 = np.matmul(w_T_cam26, center24_T_w)
+    w_T_cam37 = inverse_transform(cam37_T_w)
+    center38_T_cam37 = np.matmul(w_T_cam37, center38_T_w)
 
     # create Transforms for bounding box corners
     corners = [
-        np.matmul(center24_T_cam26, translate_transform([ 0.22,  0.134,  0.079])),
-        np.matmul(center24_T_cam26, translate_transform([ 0.22, -0.134,  0.079])),
-        np.matmul(center24_T_cam26, translate_transform([ 0.22,  0.134, -0.079])),
-        np.matmul(center24_T_cam26, translate_transform([ 0.22, -0.134, -0.079])),
-        np.matmul(center24_T_cam26, translate_transform([-0.22,  0.134,  0.079])),
-        np.matmul(center24_T_cam26, translate_transform([-0.22, -0.134,  0.079])),
-        np.matmul(center24_T_cam26, translate_transform([-0.22,  0.134, -0.079])),
-        np.matmul(center24_T_cam26, translate_transform([-0.22, -0.134, -0.079]))
+        np.matmul(center38_T_cam37, translate_transform([ 0.22,  0.134,  0.079])),
+        np.matmul(center38_T_cam37, translate_transform([ 0.22, -0.134,  0.079])),
+        np.matmul(center38_T_cam37, translate_transform([ 0.22,  0.134, -0.079])),
+        np.matmul(center38_T_cam37, translate_transform([ 0.22, -0.134, -0.079])),
+        np.matmul(center38_T_cam37, translate_transform([-0.22,  0.134,  0.079])),
+        np.matmul(center38_T_cam37, translate_transform([-0.22, -0.134,  0.079])),
+        np.matmul(center38_T_cam37, translate_transform([-0.22,  0.134, -0.079])),
+        np.matmul(center38_T_cam37, translate_transform([-0.22, -0.134, -0.079]))
     ]
 
     # find pixel coordinates of centroid
     # the Camera Pinhole model uses +x right, +y down, +z forward
-    center_3d = (-center24_T_cam26[1,3], -center24_T_cam26[2,3], center24_T_cam26[0,3])
-    camModel.fromCameraInfo(camInfo26)
+    center_3d = (-center38_T_cam37[1,3], -center38_T_cam37[2,3], center38_T_cam37[0,3])
+    camModel.fromCameraInfo(camInfo)
     center_2d = camModel.project3dToPixel(center_3d)
     center_round = (int(center_2d[0]), int(center_2d[1]))
-    # reject all future steps (drawing) on frame if the observed robot is behind the camera
-    if center_3d[2] < 0: continue
+
     # convert to OpenCV image
-    car26_image = bridge.imgmsg_to_cv2(img26, "bgr8")
+    car37_image = bridge.imgmsg_to_cv2(cam_37[idxImg][1], "bgr8")
+
+    # reject all future steps (drawing) on frame if the observed robot is behind the camera
+    if center_3d[2] < 0:
+        vid_out.write(car37_image.astype('uint8'))
+        continue
+
     # draw centroid
-    cv2.circle(car26_image, center_round, 5, (0,255,0), 3)
+    cv2.circle(car37_image, center_round, 5, (0,255,0), 3)
 
     # initialize rectangle bounds to image size
     # cv's image.shape is formatted as (height, width, length) a.k.a. (y, x, z)
-    xmin = car26_image.shape[1]
+    xmin = car37_image.shape[1]
     xmax = 0
-    ymin = car26_image.shape[0]
+    ymin = car37_image.shape[0]
     ymax = 0
 
     for corneridx, corner in enumerate(corners):
@@ -158,7 +144,7 @@ for idxImg in range(len(cam_26)):
         color = (255,255,0)
         if corneridx > 3:
             color = (0,255,255)
-        cv2.circle(car26_image, corner_round, 3, color, 2)
+        cv2.circle(car37_image, corner_round, 3, color, 2)
 
         xmin = min(xmin, corner_round[0])
         xmax = max(xmax, corner_round[0])
@@ -166,15 +152,10 @@ for idxImg in range(len(cam_26)):
         ymax = max(ymax, corner_round[1])
 
     # draw rectangle
-    cv2.rectangle(car26_image, (xmin, ymin), (xmax, ymax), (0,0,255), 1)
+    cv2.rectangle(car37_image, (xmin, ymin), (xmax, ymax), (0,0,255), 1)
 
-    # Add to processed frames list
-    images.append(car26_image)
-    h, w, l = car26_image.shape
-    img_size = (h, w)
+    # Add frame to video
+    vid_out.write(car37_image.astype('uint8'))
 
-# create video from processed frames
-vid_out = cv2.VideoWriter('centroid_' + str(delay) + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 1, (640,480))
-for i in range(len(images)):
-    vid_out.write(images[i].astype('uint8'))
+# end video
 vid_out.release()
