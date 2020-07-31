eca94cd55ab0e3218d607fe8f1f269617fcacd59
Tudor <tudorf@uw.edu>
2020-07-31 11:22:18 -0700

    new trim

--- trimbag.py
+++ trimbag.py
@@ -16,15 +16,17 @@ import cv2
 
 import matplotlib.pyplot as plt
 
-read_bag = rosbag.Bag('/home/tudorf/mushr/Car24.bag')
+read_bag = rosbag.Bag('/home/tudorf/mushr/car37_mocap.bag')
 
-write_bag = rosbag.Bag('/home/tudorf/mushr/catkin_ws/src/learning-image-geometry/car24_trim.bag', 'w')
+write_bag = rosbag.Bag('/home/tudorf/mushr/catkin_ws/src/learning-image-geometry/car37_trim.bag', 'w')
 
 start_time = None
-for topic, msg, t in read_bag.read_messages():
+for topic, msg, t in read_bag.read_messages(['/vrpn_client_node/car37/pose','/vrpn_client_node/car38/pose','/car37/camera/color/camera_info','/car37/camera/color/image_throttled', '/vrpn_client_node/car35/pose']):
     if start_time is None:
         start_time = t
-    if t - start_time > rospy.Duration(secs=30):
+    if t - start_time < rospy.Duration(secs=150):
+        continue
+    if t - start_time > rospy.Duration(secs=180):
         break
     write_bag.write(topic, msg, t=t)
 
